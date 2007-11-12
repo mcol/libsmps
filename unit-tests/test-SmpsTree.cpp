0302e83fd98f4df6478255032b277a207ef05359
Marco Colombo <m.colombo@ed.ac.uk>
2007-11-12 17:36:10 +0000

    Update testReadStocFile and start checking the number of realisations.

--- unit-tests/test-SmpsTree.cpp
+++ unit-tests/test-SmpsTree.cpp
@@ -26,15 +26,18 @@ testReadStocFile(const char StocFile[], const int expScens,
   strcpy(FileName, SmpsPath);
   strcat(FileName, StocFile);
 
-  SmpsTree tree(FileName);
-  rv = tree.readStocFile(FileName);
+  Smps tree(FileName);
+  tree.readSmpsFile();
+  tree.readCoreFile();
+  tree.readTimeFile();
+  rv = tree.readStocFile();
   if (rv) {
     return TEST_ERROR;
   }
 
   rv = checkEqual(tree.getMaxScens(), expScens, "maxScens", StocFile);
   rv = checkEqual(tree.getMaxNodes(), expNodes, "maxNodes", StocFile);
-  //  rv = checkEqual(tree.getMaxReals(), expReals, "maxReals", StocFile);
+  rv = checkEqual(tree.getMaxReals(), expReals, "maxReals", StocFile);
 
   return rv;
 }
@@ -46,35 +49,36 @@ int unitSmpsTree(void) {
 
 #ifndef SKIP_TEST_INDEP
   setFamily("testReadStocFile Indep");
-  testReadStocFile("trivial.sto", 4, 5, 4);
-  testReadStocFile("trivial2.sto", 2, 3, 2);
-  testReadStocFile("trivial4.sto", 4, 5, 4);
-  testReadStocFile("trivial5.sto", 2, 3, 2);
-  testReadStocFile("trivial-mtx.sto", 2, 3, 3);
-  testReadStocFile("fxm2_1.sto", 1, 2, 1);
-  testReadStocFile("fxm2_2.sto", 2, 3, 2);
-  testReadStocFile("fxm2_6.sto", 6, 7, 6);
-  testReadStocFile("fxm3_1.sto", 1, 3, 2);
-  testReadStocFile("fxm3_2.sto", 4, 7, 4);
-  testReadStocFile("fxm3_6.sto", 36, 43, 12);
-  testReadStocFile("fxm4_6.sto", 216, 259, 18);
+  testReadStocFile("trivial.smps", 4, 5, 9);
+  testReadStocFile("trivial2.smps", 2, 3, 3);
+  testReadStocFile("trivial4.smps", 4, 5, 5);
+  testReadStocFile("trivial5.smps", 2, 3, 3);
+  testReadStocFile("trivial-mtx.smps", 2, 3, 5);
+  testReadStocFile("fxm2_1.smps", 1, 2, 2);
+  testReadStocFile("fxm2_2.smps", 2, 3, 3);
+  testReadStocFile("fxm2_6.smps", 6, 7, 7);
+  testReadStocFile("fxm3_1.smps", 1, 3, 3);
+  testReadStocFile("fxm3_2.smps", 4, 7, 9);
+  testReadStocFile("fxm3_6.smps", 36, 43, 73);
+  testReadStocFile("fxm3_16.smps", 256, 337, 769);
+  testReadStocFile("fxm4_6.smps", 216, 259, 216);
 #endif /* SKIP_TEST_INDEP */
 
 #ifndef SKIP_TEST_BLOCK
   setFamily("testReadStocFile Blocks");
-  testReadStocFile("trivial-blk.sto", 2, 3, 0);
-  testReadStocFile("LandS_blocks.sto", 3, 4, 0);
-  testReadStocFile("mod2-2.sto", 10, 11, 0);
-  testReadStocFile("stocfor1.sto", 1, 2, 0);
-  testReadStocFile("stocfor2.sto", 64, 65, 0);
-  testReadStocFile("ft3-small.sto", 2, 3, 0);
-  testReadStocFile("pltexpA2_6.sto", 6, 7, 0);
-  testReadStocFile("pltexpA3_6.sto", 36, 43, 0);
-  testReadStocFile("pltexpA4_6.sto", 216, 259, 0);
-  testReadStocFile("pltexpA6_6.sto", 7776, 9331, 0);
-  testReadStocFile("stormG2_4.sto", 4, 5, 0);
-  testReadStocFile("stormG2_8.sto", 8, 9, 0);
-  testReadStocFile("minoux-100-0.5.sto", 100, 101, 0);
+  testReadStocFile("trivial-blk.smps", 2, 3, 5);
+  testReadStocFile("LandS-blocks.smps", 3, 4, 4);
+  testReadStocFile("mod2-2.smps", 10, 11, 101);
+  testReadStocFile("stocfor1.smps", 1, 2, 15);
+  testReadStocFile("stocfor2.smps", 64, 65, 5277);
+  testReadStocFile("ft3-small.smps", 2, 3, 65);
+  testReadStocFile("pltexpA2_6.smps", 6, 7, 43);
+  testReadStocFile("pltexpA3_6.smps", 36, 43, 505);
+  testReadStocFile("pltexpA4_6.smps", 216, 259, 4537);
+  testReadStocFile("pltexpA6_6.smps", 7776, 9331, 272161);
+  testReadStocFile("storm4.smps", 4, 5, 311);
+  testReadStocFile("storm8.smps", 8, 9, 937);
+  testReadStocFile("minoux-100-0.5.smps", 100, 101, 6601);
 #endif /* SKIP_TEST_BLOCK */
 
   return 0;
