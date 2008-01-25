434403f263dd16e235aacc9b66331221c5c22535
Marco Colombo <m.colombo@ed.ac.uk>
2008-01-25 12:38:55 +0000

    Move some code from main.cpp to SmpsOops.cpp.

--- oops-driver/SmpsOops.cpp
+++ oops-driver/SmpsOops.cpp
@@ -11,6 +11,12 @@
 
 #include <iostream>
 #include "SmpsOops.h"
+#include "oops/oopstime.h"
+#include "oops/WriteMps.h"
+
+
+double tt_start, tt_end;
+static primal_dual_pb* defineProblem(SmpsReturn *Pb);
 
 
 /** Constructor */
@@ -42,6 +48,171 @@ int SmpsOops::read() {
   rv = orderNodes();
 
   return rv;
+}
+
+/**
+ *  Call OOPS to solve the problem.
+ *
+ *  @param opt:
+ *         Command line options.
+ *  @param hopdm_options:
+ *         Pointer to the options for the solver.
+ *  @return 1 If something goes wrong; 0 otherwise.
+ */
+int SmpsOops::solve(const OptionsOops &opt, HopdmOptions *hopdm_options) {
+
+  // generate the problem
+  SmpsReturn *prob = generateSmps();
+
+  // check the validity of the arguments
+  if (!prob) {
+    cout << "No problem defined." << endl;
+    return 1;
+  }
+
+  // setup the primal-dual problem
+  primal_dual_pb *pdProb = defineProblem(prob);
+
+#ifdef REP_MEM
+  reportMem();
+#endif
+
+  // write the deterministic equivalent in mps format
+  if (opt.writeMps()) {
+    FILE *fout = fopen("smps.mps", "w");
+    Write_MpsFile(fout, pdProb->AlgAug, pdProb->b, pdProb->c, pdProb->u, 0,
+		  prob->colnames, prob->rownames);
+    fclose(fout);
+  }
+
+  printf(" --------------- oopsSolve -----------------\n");
+
+  // options for the complete problem
+  hopdm_options->glopt->conv_tol = 1.e-4;
+
+  hopdm_ret *ret;
+  PrintOptions *Prt = NewHopdmPrt(PRINT_ITER);
+
+  if (opt.dontSolve()) {
+    printf("Problem not solved by request.\n");
+    goto TERMINATE;
+  }
+
+  // start the clock
+  tt_start = oopstime();
+
+  // solve the problem
+  ret = hopdm(printout, pdProb, hopdm_options, Prt);
+
+  // stop the clock
+  tt_end = oopstime();
+
+  // report statistics
+  fprintf(printout, "Elapsed time: %.10g seconds.\n", tt_end - tt_start);
+
+  if (opt.printSolution())
+    getSolution(pdProb, prob);
+
+  // clean up
+  free(ret);
+
+ TERMINATE:
+
+  // clean up
+  FreePDProblem(pdProb);
+  freeSmpsReturn(prob);
+  delete Prt;
+
+  return 0;
+}
+
+/**
+ *  Set up the OOPS Algebras and Vectors and build the primal-dual problem.
+ *
+ *  @param Pb:
+ *         Pointer to an SmpsReturn structure.
+ *  @return Pointer to a primal_dual_pb structure containing the problem
+ *          to be solved by OOPS.
+ *
+ *  @note
+ *  InitAlgebrasNew assumes that a callback function is set up for all
+ *  SparseMatrix leaves of A and Q.
+ */
+primal_dual_pb* defineProblem(SmpsReturn *Pb) {
+
+  Algebra *A = Pb->AlgA;
+  Algebra *Q = Pb->AlgQ;
+
+  printf(" --------------- InitAlgebras --------------\n");
+  Algebra *AlgAug = InitAlgebrasNew(A, Q);
+
+  Vector *vb = NewVector(A->Trow, "vb");
+  Vector *vc = NewVector(A->Tcol, "vc");
+  Vector *vu = NewVector(A->Tcol, "vu");
+
+  Vector *vx = NewVector(A->Tcol, "vx");
+  Vector *vy = NewVector(A->Trow, "vy");
+  Vector *vz = NewVector(A->Tcol, "vz");
+
+  CopyDenseToVector(Pb->b, vb);
+  CopyDenseToVector(Pb->c, vc);
+  CopyDenseToVector(Pb->u, vu);
+
+  return NewPDProblem(AlgAug, vb, vc, vu, vx, vy, vz);
+}
+
+/**
+ *  Retrieve the solution.
+ *
+ *  @param Prob:
+ *         Pointer to the problem solved by OOPS.
+ *  @param Ret:
+ *         Pointer to an SmpsReturn structure.
+ *
+ *  @bug
+ *  The value of the slacks is always zero, as the slacks would need to be
+ *  computed here.
+ */
+int SmpsOops::getSolution(primal_dual_pb *Prob, SmpsReturn *Ret) {
+
+  DenseVector *x, *y, *z, *r;
+  Tree *Trow = Ret->AlgA->Trow;
+  Tree *Tcol = Ret->AlgA->Tcol;
+  int nRows  = Trow->end - Trow->begin;
+  int nCols  = Tcol->end - Tcol->begin;
+
+  // allocate space for the solution vectors
+  x = NewDenseVector(nCols, "dx");
+  y = NewDenseVector(nRows, "dy");
+  z = NewDenseVector(nCols, "dz");
+
+  // this is just a placeholder, we cannot yet compute the slacks
+  r = NewDenseVector(nRows, "slacks");
+
+  // recover initial order on solution vectors
+  SmpsVectorToDense(Prob->x, x, Ret, ORDER_COL);
+  SmpsVectorToDense(Prob->y, y, Ret, ORDER_ROW);
+  SmpsVectorToDense(Prob->z, z, Ret, ORDER_COL);
+
+  // print the solution
+  const NodeInfo *info = getNodeInfo();
+
+#ifdef WITH_MPI
+  if (IS_ROOT_PAR)
+#endif
+    printSolution(info, x->elts, y->elts, r->elts, z->elts);
+
+  // clean up
+  delete[] info->nRowsNode;
+  delete[] info->nColsNode;
+  delete info;
+
+  FreeDenseVector(x);
+  FreeDenseVector(y);
+  FreeDenseVector(z);
+  FreeDenseVector(r);
+
+  return 0;
 }
 
 /** Return information on the nodes necessary for printing the solution */
