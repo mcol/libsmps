379946cd80846b05424cbbc8c88f1b89f21b04c4
Marco Colombo <m.colombo@ed.ac.uk>
2008-02-27 13:10:36 +0000

    Advance the implementation of the Node class.
    This makes the Node class usable. It's not complete (it lacks the scenario
    changes information, for example), but it works.

--- Node.cpp
+++ Node.cpp
@@ -15,29 +15,63 @@ using namespace std;
 
 
 /** Constructor */
-Node::Node() :
-  level(0),
-  nChildren(0),
-  parent(NULL) {
+Node::Node(const int nodeName) :
+  _parent(NULL),
+  _name(nodeName),
+  _level(0),
+  _scenario(0),
+  _probNode(0.0),
+  _firstRow(0),
+  _firstCol(0),
+  _nRows(0),
+  _nCols(0) {
 }
 
 /** Destructor */
 Node::~Node() {
 
   // free the space occupied by the children nodes
-  for (vector<Node *>::iterator childNode = children.begin();
-       childNode != children.end(); ++childNode) {
+  for (vector<Node *>::iterator childNode = _children.begin();
+       childNode != _children.end(); ++childNode) {
     delete *childNode;
   }
 }
 
 /** Add a node to the list of children */
-int Node::addChild() {
+int Node::addChildNode(Node *childNode) {
 
-  Node *child = new Node;
-  child->level = level + 1;
-  child->parent = this;
-  children.push_back(child);
+  // link the child to the parent
+  childNode->_level = _level + 1;
+  childNode->_parent = this;
+
+  // store the new child
+  _children.push_back(childNode);
 
   return 0;
+}
+
+/** Retrieve the child node at the given index */
+Node* Node::getChild(const int childNumber) const {
+
+  // check that the index provided is sensible
+  if (childNumber > (int) _children.size() ||
+      childNumber < 0)
+    return NULL;
+
+  return _children[childNumber];
+}
+
+/** Print some of the information of the node */
+void Node::printNode() const {
+
+  // node parent scen n_chd per prob | rows cols
+  printf("  %4i  %4i  %4i  %4i  %4i   %.4f  | %4i  %4i\n",
+	 _name,
+	 _parent ? _parent->name() : 0,
+	 _scenario,
+	 nChildren(),
+	 _level + 1,
+	 _probNode,
+	 _nRows,
+	 _nCols);
 }
--- Node.h
+++ Node.h
@@ -22,32 +22,99 @@ class Node {
  public:
 
   /** Constructor */
-  Node();
+  Node(const int name);
 
   /** Destructor */
   ~Node();
 
   /** Add a node to the list of children */
-  int addChild();
+  int addChildNode(Node *child);
 
-  /** Set the parent node */
-  void setParent(Node *parentNode) {
-    parent = parentNode;
+  /** Retrieve the child node at the given index */
+  Node *getChild(const int childNumber) const;
+
+  // Setters
+
+  /** Set the scenario index */
+  void setScen(const int scen) {
+    _scenario = scen;
   }
+
+  /** Set the path probability for this node */
+  void setProb(const double prob) {
+    _probNode = prob;
+  }
+
+  /** Set the indices to the matrix data */
+  void setMatrixPointers(const int row, const int col,
+			 const int nRows, const int nCols) {
+    _firstRow = row;
+    _firstCol = col;
+    _nRows = nRows;
+    _nCols = nCols;
+  }
+
+  // Accessor functions
+
+  /** Return the name of the node */
+  int name(void) const {
+    return _name;
+  }
+
+  /** Return the number of children of the node */
+  int nChildren(void) const {
+    return _children.size();
+  }
+
+  /** Return the level of the node in the tree */
+  int level(void) const {
+    return _level;
+  }
+
+  /** Return the scenario index of the node */
+  int scenario(void) const {
+    return _scenario;
+  }
+
+  /** Return the probability associated to the node */
+  double probNode(void) const {
+    return _probNode;
+  }
+
+  /** Print some of the information of the node */
+  void printNode(void) const;
 
  private:
 
-  /** The level of the node within a tree */
-  int level;
-
-  /** The number of children of the node */
-  int nChildren;
-
   /** Pointer to the parent node */
-  Node *parent;
+  Node *_parent;
 
-  /** The set of pointers to the children nodes */
-  vector<Node*> children;
+  /** Set of pointers to the children nodes */
+  vector<Node*> _children;
+
+  /** Index name of this node */
+  int _name;
+
+  /** Level of the node within a tree */
+  int _level;
+
+  /** Scenario this node belongs to */
+  int _scenario;
+
+  /** Path probability of this node */
+  double _probNode;
+
+  /** Index of the first row of node (in the deterministic equivalent) */
+  int _firstRow;
+
+  /** Index of the first column of node (in the deterministic equivalent) */
+  int _firstCol;
+
+  /** Number of rows for this node */
+  int _nRows;
+
+  /** Number of columns for this node */
+  int _nCols;
 
 };
 
